7086b0fe4ad737f062e87b71290c74fb8e00456f
taylor1003 <apple@appledeMacBook-Pro.local>
2015-04-10 23:41:10 +0800

    Initial Commit

---
+++ AFFileCache/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  AFFileCache
+//
+//  Created by TaoPing on 15-4-10.
+//  Copyright (c) 2015年 TaoPing. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
